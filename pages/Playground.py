51ddb2ffb11f1cd788dd541e6e640335e7008b4a
Mackaber <Mackaber@users.noreply.github.com>
2023-09-22 06:34:57 +0000

    thing

---
+++ pages/Playground.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import streamlit as st
+
+data_df = pd.DataFrame(
+    {
+        "widgets": ["st.selectbox", "st.number_input", "st.text_area", "st.button"],
+        "favorite": [True, False, False, True],
+    }
+)
+
+edited = st.data_editor(
+    data_df,
+    column_config={
+        "favorite": st.column_config.CheckboxColumn(
+            "Your favorite?",
+            help="Select your **favorite** widgets",
+            default=True,
+        )
+    },
+    disabled=["widgets"],
+    hide_index=True,
+)
+
+edited["favorite"][0]
